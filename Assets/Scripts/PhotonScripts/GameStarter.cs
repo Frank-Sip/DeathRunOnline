e8d96ee4eb852e13ba85874b121da8d5b5f6e885
Nachomulligan <ignaciodiazmulligan@gmail.com>
2025-08-31 17:03:50 -0300

    change game starter

--- Assets/Scripts/PhotonScripts/GameStarter.cs
+++ Assets/Scripts/PhotonScripts/GameStarter.cs
@@ -1,6 +1,7 @@
-using UnityEngine;
+﻿using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections;
 using System.Collections.Generic;
 
 public class GameStarter : MonoBehaviourPunCallbacks
@@ -9,19 +10,42 @@ public class GameStarter : MonoBehaviourPunCallbacks
     [SerializeField] private Transform playerSpawnPoint;
     [SerializeField] private List<Transform> playerSpawnPoints = new List<Transform>();
     private int currentSpawnIndex = 0;
+    private bool hasSpawned = false;
 
     private void Start()
     {
-        PhotonNetwork.JoinRandomRoom();
+        StartCoroutine(CheckRoomStatus());
     }
 
-    public override void OnJoinRandomFailed(short returnCode, string message)
+    private IEnumerator CheckRoomStatus()
     {
-        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 4 });
+        yield return null; 
+        
+        if (PhotonNetwork.InRoom && !hasSpawned)
+        {
+            SpawnPlayer();
+        }
+        else if (!PhotonNetwork.InRoom)
+        {
+            Debug.LogWarning("Not in room when GameStarter started. This shouldn't happen.");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+        }
     }
 
     public override void OnJoinedRoom()
     {
+        if (!hasSpawned)
+        {
+            Debug.Log("GameStarter: Joined Room - Spawning Player");
+            SpawnPlayer();
+        }
+    }
+
+    private void SpawnPlayer()
+    {
+        if (hasSpawned) return;
+        
+        hasSpawned = true;
         Transform spawn = GetPlayerSpawnPosition();
         PhotonNetwork.Instantiate(playerPrefab.name, spawn.position, spawn.rotation, 0);
     }

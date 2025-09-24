f69cc3745907f714143c0a5124d79c2509df7b33
Nachomulligan <ignaciodiazmulligan@gmail.com>
2025-09-24 11:42:17 -0300

    Add Projectile script and update trap handling

--- Assets/Scripts/Traps/GenericTrap.cs
+++ Assets/Scripts/Traps/GenericTrap.cs
@@ -1,27 +1,38 @@
-using UnityEngine;
+﻿using UnityEngine;
 using Photon.Pun;
 
 public class GenericTrap : MonoBehaviourPun
 {
     [SerializeField] private bool destroyOnImpact = true;
-    
+
     private void OnCollisionEnter(Collision collision)
     {
+        if (!photonView.IsMine) return;
+
         IDamageable damageable = collision.gameObject.GetComponent<IDamageable>();
         if (damageable != null)
         {
-            photonView.RPC("RPC_HandleCollision", RpcTarget.All, collision.gameObject.GetComponent<PhotonView>().ViewID);
+            PhotonView targetView = collision.gameObject.GetComponent<PhotonView>();
+            if (targetView != null)
+            {
+                photonView.RPC("RPC_HandleCollision", RpcTarget.All, targetView.ViewID);
+            }
         }
-        
-        if (!destroyOnImpact) return;
-        PhotonNetwork.Destroy(gameObject);
+
+        if (destroyOnImpact)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
     }
 
     [PunRPC]
     private void RPC_HandleCollision(int viewID)
     {
         PhotonView targetPhotonView = PhotonView.Find(viewID);
-        IDamageable damageable = targetPhotonView.GetComponent<IDamageable>();
-        damageable.Die();
+        if (targetPhotonView != null)
+        {
+            IDamageable damageable = targetPhotonView.GetComponent<IDamageable>();
+            damageable?.Die();
+        }
     }
 }
---
+++ Assets/Scripts/Traps/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(PhotonView))]
+public class Projectile : MonoBehaviourPun
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (photonView.IsMine)
+        {
+            rb.isKinematic = false;
+        }
+        else
+        {
+            rb.isKinematic = true; // otros clientes solo ven sync
+        }
+    }
+}
